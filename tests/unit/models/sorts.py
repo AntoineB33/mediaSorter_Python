6007bf5e8659c45afe18345f776f5ddd8829e34a
AntoineB33 <antoine.barbault@gmail.com>
2025-07-10 20:45:21 +0200

    Refactor find_valid_sortings; remove redundant code and improve segment parsing logic

--- tests/unit/models/sorts.py
+++ tests/unit/models/sorts.py
@@ -1,4 +1,5 @@
-from src.models.generate_sortings import find_valid_sortings, order_table
+from src.models.generate_sortings import order_table
+from src.models.find_valid_sortings import find_valid_sortings
 
 table = [
     ["_[1]-3_", "as far as possible from 2"],
@@ -6,10 +7,14 @@ table = [
     [],
     []
 ]
+table = [
+    ["_[1]-1_"],
+    []
+]
 res = find_valid_sortings(table)
 if type(res) is str:
     print(res)
 else:
     print("Found valid sortings:", res)
-    ordered_res = order_table(res, table)
+    ordered_res = order_table(res[0], table)
     print(ordered_res)
